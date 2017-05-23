4954dc12a9be21f3a0e7611ef2e85da95812f744
sisilekoblitz <sisile.koblitz@fyii.de>
2017-05-23 18:45:05 +0200

    Add files via upload

--- test.py
+++ test.py
@@ -8,16 +8,19 @@ from tweet_text import idle_text
 # In unittest, test cases are represented by instances of unittest's TestCase class.
 # To make your own test cases you must write subclasses of TestCase.
 
-
 class TestTweetText(unittest.TestCase):
+    # This is called immediately before calling each test method
+    def setUp(self):
+        self.text = idle_text()
 
-    def test_idle(self):
+    def test_start(self):
         # In order to test something, we use one of the assert*()
         # methods provided by the TestCase base class
         # https://docs.python.org/2/library/unittest.html#unittest.TestCase.assertTrue
-        self.assertTrue( isinstance(idle_text(), str) )
+        self.assertTrue(self.text.startswith('It is'))
 
+    def test_end(self):
+        self.assertTrue(self.text.endswith('.'))
 
-
-#if __name__ == '__main__':
-#    unittest.main(verbosity=2)
+if __name__ == '__main__':
+    unittest.main(verbosity=2)
--- tweet_text.py
+++ tweet_text.py
@@ -4,7 +4,6 @@
 from datetime import datetime
 # convert times according to time zones
 from pytz import timezone
-from requests import get
 
 def reply(tweet):
     """Return text to be used as a reply"""
@@ -17,9 +16,8 @@ def reply(tweet):
     return None
 
 def idle_text():
-    # Some more ideas: https://www.programmableweb.com/category/humor/api
-    data = get('https://api.chucknorris.io/jokes/random').json()
-    joke = data['value']
-    return joke[0:139]
-
-
+    """Return text that is tweeted when not replying"""
+    # Construct the text we want to tweet out (140 chars max)
+    berlin_time = datetime.now(timezone('Europe/Berlin'))
+    text = berlin_time.strftime("It is %H:%M:%S on a %A (%d-%m-%Y).")
+    return text
